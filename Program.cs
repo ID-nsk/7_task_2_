acf0cfe85fc8d981736f5f03d60f4961c200f9d8
Ivan Dyakov <i.dyakov@sibstrin.ru>
2021-11-30 16:47:05 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Laba4
+{
+    class Program
+    {
+        // Метод для вычисления объема и площади поверхности куба по длине его ребра.
+        static void CubeVS(double a)
+        {
+            double v = Math.Pow(a, 3);
+            double s = (Math.Pow(a, 2)) * 6;
+            Console.WriteLine($"Площадь куба = {s}");
+            Console.WriteLine($"Объём куба = {v}");
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение ребра куба: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            CubeVS(a);
+            Console.ReadKey();
+        }
+    }
+}
